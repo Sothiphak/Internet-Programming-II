4a13cd9a5accd308e60cf68c684ee04c502f1c6e
Sothiphak <sopheapsothiphak@gmail.com>
2026-04-02 23:11:41 +0700

    Add RabbitMQ to OrdersService to match instructor's code

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,13 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
 import { NotificationsService } from '../notifications/notifications.service';
 
 @Injectable()
 export class OrdersService {
   constructor(
+    @Inject('ORDERS_SERVICE') private client: ClientProxy,
     private readonly notifications: NotificationsService,
   ) {}
 
   createOrder(orderDto: any) {
+    this.client.emit('order_created', { order: orderDto, createdAt: new Date().toISOString() });
+
     this.notifications.notify('order_created', {
       order: orderDto,
     });
